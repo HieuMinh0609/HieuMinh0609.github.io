3201787cf4f6bab6bbf6f03fe2d480df6ea80f4e
HieuMinh0609 <47945591+HieuMinh0609@users.noreply.github.com>
2020-08-13 15:34:35 +0700

    upate
    fds

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ function playStream(idVideoTag,stream){
 // openStream()
 // .then(stream => playStream('localStream',stream))
 
-const peer = new Peer();
-
+//const peer = new Peer();
+const peer = new Peer({key:'peerjs',host:'sream-3005.herokuapp.com',secure:true,port:443});
 peer.on('open',id=>{
     $('#my-peer').append(id)
     $('#btnSignUp').click(() =>{
