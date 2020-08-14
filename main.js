9002397a97efb1994cdfc8f5ce364ced3f779688
HieuMinh0609 <47945591+HieuMinh0609@users.noreply.github.com>
2020-08-14 10:16:46 +0700

    fdsf
    sdfsdfsd

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
  const socket = io('https://stream301.herokuapp.com/');
- //const socket = io('http://localhost:3000');
+ //const socket = io('http://localhost:3000/');
+
 $('#div-chat').hide();
 
 
@@ -26,24 +27,21 @@ $.ajax({
 socket.on('DANH_SACH_ONLINE',users =>{
     $('#div-chat').show();
     $('#div-dang-ky').hide();
-   var userss = users;
+   
     users.forEach(user => {
         const {ten ,peerId} = user;
         $('#listUser').append(`<li id="${peerId}">${ten}</li>`)
-        showAll(users,user)
     });
 
     socket.on('CO_NGUOI_MOI',user =>{
         const {ten ,peerId} = user;
-        $('#listUser').append(`<li id="${peerId}">${ten}</li>`);
-        showAll(userss,user);
+        $('#listUser').append(`<li id="${peerId}">${ten}</li>`)
     })
-  
+ 
     socket.on('AI_DO_NGAT_KET_NOI',id =>{
         console.log(id);
         console.log(`#${id}`);
         $(`#${id}`).remove();
-        $(`#remote${id}`).remove();
      })
 });
 
@@ -51,40 +49,6 @@ socket.on('DANG_KY_THAT_BAI',() =>{
     alert('fail!')
 })
 
-function showAll(users,user){
-   
-    if(users.length!==1){
-        $('.removeStreamPerson').append('<video style="margin-right:10px" class="col-md-4" id="remote'+user.peerId+'" width="300" controls></video>')
-        console.log(user.peerId)
-        console.log(users[0].peerId)
-        openStream()
-        .then(stream => {
-            playStream('localStream',stream);
-            const call = peer.call(users[0].peerId,stream);
-            call.on('stream',remoteStream => playStream("remote"+user.peerId,remoteStream));
-
-            peer.on('call',call => {
-                openStream()
-                .then(stream => {
-                    call.answer(stream);
-                    playStream('localStream',stream);
-                    call.on('stream',remoteStream => playStream("remote"+user.peerId,remoteStream));
-                })
-            })
-        });
-    }
-    else{
-        peer.on('call',call => {
-            openStream()
-            .then(stream => {
-                call.answer(stream);
-                playStream('localStream',stream);
-               // call.on('stream',remoteStream => playStream("remote"+user.peerId,remoteStream));
-            })
-        })
-    }
-   
-}
 
 
 function openStream(){
@@ -105,7 +69,7 @@ function playStream(idVideoTag,stream){
 
 //const peer = new Peer();
 //const peer = new Peer({key:'peerjs',host:'sream-3005.herokuapp.com',secure:true,port:443});
-const peer = new Peer({host:'sream-3005.herokuapp.com',secure:true,port:443,config:customConfig});
+ const peer = new Peer({host:'sream-3005.herokuapp.com',secure:true,port:443,config:customConfig});
 peer.on('open',id=>{
     $('#my-peer').append(id)
     $('#btnSignUp').click(() =>{
@@ -124,12 +88,12 @@ $('#btnCall').click(()=>{
     });
 })
 
-// peer.on('call',call => {
-//     openStream()
-//     .then(stream => {
-//         call.answer(stream);
-//         playStream('localStream',stream);
-//         call.on('stream',remoteStream => playStream('remoteStream',remoteStream));
-//     })
-// })
+peer.on('call',call => {
+    openStream()
+    .then(stream => {
+        call.answer(stream);
+        playStream('localStream',stream);
+        call.on('stream',remoteStream => playStream('remoteStream',remoteStream));
+    })
+})
 
